b8d004b20e80b8ee38e611a4b5536580fe5fda79
Aryan <bhardwaj7a@gmail.com>
2025-08-11 00:50:41 +0530

    Write The Ui logic for the top doctors component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import SpecialityMenu from "../components/SpecilaityMenu";
+import TopDoctors from "../components/TopDoctors";
 
 const Home = () => {
     return (
         <div>
             <Header />
             <SpecialityMenu />
+            <TopDoctors />
         </div>
     )
 }
