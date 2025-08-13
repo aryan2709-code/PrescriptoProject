bc51d698bf5ca9d9e2de4c59fb19a0bd6d0132a3
Aryan <bhardwaj7a@gmail.com>
2025-08-13 12:17:37 +0530

    Added a config file for connecting to the mongoDB database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config"
+import connectDB from "./config/mongodb.js";
 
 //  app config
 const app = express(); // Creating an instance of the express server named app
 const port = process.env.PORT || 4000
+connectDB();
 
 // middlewares
 app.use(express.json())
