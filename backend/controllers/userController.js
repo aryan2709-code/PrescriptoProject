d76b2755d7089ef3e18759809b64870ab8c3e9a6
Aryan <bhardwaj7a@gmail.com>
2025-08-15 02:17:28 +0530

    Added API endpoint to get the user profile from the database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -94,6 +94,17 @@ const loginUser = async (req,res) => {
     }
 }
 
-// API to 
+// API to get user Profile data
+const getProfile = async (req,res) => {
+    try {
+        const { userId } = req.body;
+        const userData = await userModel.findById(userId).select('-password');
 
-export {registerUser,loginUser};
+        res.json({success:true, userData})
+    } catch (error) {
+        console.log(error);
+        res.json({success : false, message : error.message})
+    }
+}
+
+export {registerUser,loginUser,getProfile};
