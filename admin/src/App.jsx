9cef0988e57e9f3fcf3c6ab61346ee7bfacf0360
Aryan <bhardwaj7a@gmail.com>
2025-08-14 18:15:51 +0530

    Wired the LogIn page to the backend and created the navbar component for the admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,24 @@
+import { useContext } from "react";
 import Login from "./pages/Login";
+import {ToastContainer,toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
+
 
 const App = () => {
-  return(
-    <div>
-      <Login />
+const {aToken} = useContext(AdminContext);
+
+  return aToken ? (
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer />
+      <Navbar />
     </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
   )
 }
 
