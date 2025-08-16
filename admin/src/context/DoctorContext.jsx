6b3592470b87bba9f9871bd1492aee217b8e2104
Aryan <bhardwaj7a@gmail.com>
2025-08-16 16:47:32 +0530

    Completed the backend and the frontend logic for doctor dashboard data

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -9,6 +9,7 @@ const DoctorContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [dToken, setDToken] = useState(localStorage.getItem("dToken") ? localStorage.getItem("dToken") : "");
     const [appointments, setAppointments] = useState([]);
+    const [dashData,setDashData] = useState(false);
 
     const getAppointments = async () => {
         try {
@@ -64,8 +65,26 @@ const DoctorContextProvider = (props) => {
         }
     }
 
+    const getDashData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + "/api/doctor/dashboard",{headers:{dToken}});
+            if(data.success)
+            {
+                setDashData(data.dashData);
+                console.log(data.dashData);
+            }
+            else
+            {
+                toast.error(data.message)
+            }
+        } catch (error) {
+             console.log(error)
+            toast.error(error.message)
+        }
+    }
+
     const value = {
-         dToken,setDToken,backendUrl,appointments,setAppointments,getAppointments,completeAppointment,cancelAppointment
+         dToken,setDToken,backendUrl,appointments,setAppointments,getAppointments,completeAppointment,cancelAppointment,dashData,setDashData,getDashData
     }
 
 
